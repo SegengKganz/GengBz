72acd27f4a58169d70568b94aaac1264682029f5
SegengKganz <segengelek@gmail.com>
2023-07-28 09:22:45 +0700

    first commit

---
+++ plugins/donasi.js
@@ -0,0 +1,27 @@
+let handler = async (m, { conn, usedPrefix: _p, __dirname, text, command }) => {
+  let don = 'media/qr.png'
+  let cap = `▷ DONASI BOT ◁
+▪ Pulsa: ${global.ppulsa}
+▫ Pulsa: ${global.ppulsa2}
+▫ Dana: ${global.pdana}
+▫ OVO: ${global.povo}
+▪ Gopay: ${global.pgopay}
+
+Semoga Anda diberikan kemudahan rezeki dan dilipatgandakan rezeki Anda.`
+conn.sendMessage(m.chat, {
+text: cap,
+contextInfo: {
+externalAdReply: {
+title: '© GengBotzz',
+body: 'D O N A T E',
+thumbnailUrl: "https://telegra.ph/file/811e4fca56af83401479d.jpg",
+mediaType: 1,
+renderLargerThumbnail: true
+}}}, { quoted: m}) 
+}
+
+handler.help = ['donasi'];
+handler.tags = ['info'];
+handler.command = /^(donasi)$/i;
+
+export default handler;
